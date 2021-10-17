c295c5cae83a6d7cc6c721187fc6f618021b43bf
wichru <maciej.wicher@netguru.co>
2021-10-18 00:51:52 +0200

    :rocket:

---
+++ README.md
@@ -0,0 +1,26 @@
+# patchwork_ruby
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+This is the ROR solution of Patchwork tech task.
+
+### Technology stack
+
+* [Ruby 3.0.2]
+* [Rails 6.1.4]
+* [RSpec] - Rspec tests
+* [Rubocop] - Ruby static code analyzer
+
+### Installation
+
+```sh
+$ cd patchwork_ruby
+$ bundle install
+```
+
+### Tests
+
+```sh
+$ cd patchwork_ruby
+$ rspec spec
+```
---
+++ spec/rails_helper.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+require 'vcr'
+require 'webmock/rspec'
+require 'pry'
+require 'rspec/autorun'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+#
+# The following line is provided for convenience purposes. It has the downside
+# of increasing the boot-up time by auto-requiring all files in the support
+# directory. Alternatively, in the individual `*_spec.rb` files, manually
+# require only the support files necessary.
+#
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
+
+VCR.configure do |config|
+  config.before_record do |i|
+    i.request.headers.delete('Authorization')
+  end
+  config.allow_http_connections_when_no_cassette = false
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+
+  vcr_mode = /rec/i.match?(ENV['VCR_MODE']) ? :all : :once
+
+  config.default_cassette_options = {
+    record: vcr_mode,
+    match_requests_on: %i[method uri body]
+  }
+end
