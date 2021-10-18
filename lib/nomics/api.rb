b2f1fbd6538a728197234e885b471792c1abccb1
wichru <maciej.wicher@netguru.co>
2021-10-18 06:21:48 +0200

    Update docs

--- lib/nomics/api.rb
+++ lib/nomics/api.rb
@@ -3,7 +3,7 @@
 module Nomics
   # Nomics API wrapper class to get in to specific endopint
   class Api
-    # Get the full payload of cryptocurrencies defined by ids [Array<Strings>]
+    # Get the full payload of cryptocurrencies defined by ids [<[String]>]
     #
     # @param ids [<[String]>] cryptocurrencies ticker symbols
     # @return [<[Hash]>] with full payload
@@ -11,6 +11,11 @@ module Nomics
       client.get('currencies/ticker', { ids: ids.join(',') })
     end
 
+    # Get the full payload of cryptocurrencies defined by ids [<[String]>] exchanged to given currency [<String>]
+    #
+    # @param currency [<String>] the symbol of the currency or fiat to be exchanged
+    # @param ids [<[String]>] cryptocurrencies ticker symbols
+    # @return [<[Hash]>] with full payload
     def convert_crypto(currency:, ids: [])
       client.get('currencies/ticker', { convert: currency.to_s, ids: ids.join(',') })
     end
